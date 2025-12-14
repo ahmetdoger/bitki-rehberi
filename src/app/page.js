b23bc634e88c599ed32678a369eebdebd66daeef
ahmetdoger <ce.ahmetdoger@gmail.com>
2025-12-14 21:48:20 +0300

    git

---
+++ src/app/page.js
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+export default function Home() {
+
+
+  return (
+    
+    <main className="container py-5 ">
+      <nav className="navbar bg-body-tertiary">
+  <div className="container-fluid">
+    <form class="container-fluid justify-content-start">
+     <span className="navbar-brand mb-0 h1">Bitki Rehberi</span> 
+      <button className="btn btn-sm btn-outline-secondary" type="button">Smaller button</button>
+    </form>
+  </div>
+
+</nav>
+     
+    </main>
+  );
+}
